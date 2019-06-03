8ab33c3f188c79b60de2849b11d566877781b97d
Jacek Pobiarżyn <jacek.pobiarzyn@gmial.com>
2019-06-03 10:14:32 +0200

    Updates

---
+++ precache-manifest.e56467ad4e2682a80b940dfe092b9735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabf5151af73d84160999cb06bdc509a",
+    "url": "/toDo_app_React/./index.html"
+  },
+  {
+    "revision": "e88f939bc09adf14e4b6",
+    "url": "/toDo_app_React/./static/css/main.1a0f5503.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/toDo_app_React/./static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "e88f939bc09adf14e4b6",
+    "url": "/toDo_app_React/./static/js/main.571e61bb.chunk.js"
+  },
+  {
+    "revision": "4ecb722530109838f7c6",
+    "url": "/toDo_app_React/./static/js/runtime~main.f6c8047a.js"
+  },
+  {
+    "revision": "4d24e122720d6118390c9edabc7f61fc",
+    "url": "/toDo_app_React/./static/media/bg.4d24e122.jpg"
+  }
+]);
